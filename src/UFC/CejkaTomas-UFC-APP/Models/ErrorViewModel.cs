42e40a96db949a811e011a19a9572a3772d8a39e
TomasCejka06 <cejkatomas@sssvt.cz>
2026-01-05 11:02:16 +0100

    start

---
+++ src/UFC/CejkaTomas-UFC-APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CejkaTomas_UFC_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
